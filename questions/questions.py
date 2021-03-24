9aa84ddd53d0347991d5a7f19ad6410e3d5179b5
hodoug <j.hodoug@gmail.com>
2021-03-24 12:41:20 +0900

    Update questions.py

--- questions/questions.py
+++ questions/questions.py
@@ -20,7 +20,6 @@
 #
 # 5. 2-2
 # data_url = 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip'
-# 이미지 파일에서 오류가 생김.
 #
 # 6. 2-4
 # sign-mnist
