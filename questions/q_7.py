8afb23e5938b6bfd78fa68a542a843fa36231334
hodoug <j.hodoug@gmail.com>
2021-03-24 21:13:36 +0900

    Update q_7.py

--- questions/q_7.py
+++ questions/q_7.py
@@ -1,7 +1,4 @@
 # rps
-# train_url = https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps.zip
-# valid_url = https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps-test-set.zip
-
 
 import tensorflow as tf
 from tensorflow.keras import layers, Input
@@ -10,6 +7,12 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 import os
 import numpy as np
 import matplotlib.pyplot as mpplot
+
+
+def clear_session():
+    tf.keras.backend.clear_session()
+    tf.random.set_seed(51)
+    np.random.seed(51)
 
 
 def flat_histories(histories):
@@ -31,6 +34,18 @@ def plot_history(history, metrics=('loss',)):
     mpplot.legend()
 
 
+def plot(ys, labels=None):
+    mpplot.figure(figsize=(10, 6))
+    x = range(len(ys[0]))
+    for i in range(len(ys)):
+        if labels is not None and len(labels) == len(ys):
+            label = labels[i]
+        else:
+            label = None
+        mpplot.plot(x, ys[i], label=label)
+    mpplot.legend()
+
+
 def callback_of_stop_training(condition, message='Cancel training...'):
     class StopTraining(tf.keras.callbacks.Callback):
         def on_epoch_end(self, epoch, logs=None):
@@ -41,7 +56,7 @@ def callback_of_stop_training(condition, message='Cancel training...'):
     return StopTraining()
 
 
-# Get Data
+# Gat Data
 def load_image(directory=None, nth=0, path=None, target_size=None):
     if path is None:
         image_path = os.path.join(directory, os.listdir(directory)[0])
@@ -50,7 +65,7 @@ def load_image(directory=None, nth=0, path=None, target_size=None):
     return load_img(image_path, target_size=target_size)
 
 
-print(load_image('tmp/rps/rps/paper', 0))
+print(load_image('tmp/rps/rps/rock', 0))
 
 
 # Prepare Data
@@ -63,6 +78,7 @@ def image_generators_dir(dir_train, dir_valid, target_size, class_mode, rescale,
         zoom_range=0.2,
         horizontal_flip=True,
         rotation_range=40
+
     )
     gen_train = datagen_train.flow_from_directory(
         dir_train,
@@ -80,21 +96,20 @@ def image_generators_dir(dir_train, dir_valid, target_size, class_mode, rescale,
     return gen_train, gen_valid
 
 
-dir_train = 'tmp/rps/rps'
-dir_valid = 'tmp/rps/rps-test-set'
-
-target_size = (150, 150)
+dir_train, dir_valid, target_size = 'tmp/rps/rps', 'tmp/rps/rps-test-set', (75, 75)
 input_shape = (*target_size, 3)
 
 gen_train, gen_valid = image_generators_dir(
-    dir_train, dir_valid, target_size, class_mode='categorical', rescale=1/255, batch_size=32
+    dir_train, dir_valid, target_size,
+    class_mode='categorical', rescale=1/255, batch_size=32
 )
 
 
 # Model
+clear_session()
 model = tf.keras.models.Sequential([
     Input(shape=input_shape),
-    layers.Conv2D(16, (3, 3), activation='relu', ),
+    layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D(pool_size=(2, 2)),
     # layers.Conv2D(32, (3, 3), activation='relu'),
     # layers.MaxPooling2D(pool_size=(2, 2)),
@@ -103,7 +118,7 @@ model = tf.keras.models.Sequential([
     # layers.Conv2D(64, (3, 3), activation='relu'),
     # layers.MaxPooling2D(pool_size=(2, 2)),
     layers.Flatten(),
-    layers.Dense(1024, activation='relu'),
+    layers.Dense(32, activation='relu'),
     # layers.Dropout(0.2),
     layers.Dense(3, activation='softmax')
 ])
@@ -116,14 +131,20 @@ histories = []
 
 
 # Train
-stop_training = callback_of_stop_training(lambda logs: logs.get('acc') > 0.85 and logs.get('val_acc') > 0.85)
-history = model.fit(gen_train, validation_data=gen_valid, epochs=5, callbacks=[stop_training])
+epoch_unit = 10
+stop_training = callback_of_stop_training(lambda logs: logs.get('acc') > 0.8 and logs.get('val_acc') > 0.8)
+history = model.fit(gen_train, validation_data=gen_valid, epochs=epoch_unit)
 histories.append(history.history)
+plot_history(history.history, ('acc', 'val_acc'))
 
 
 # History
 saved_history = flat_histories(histories)
 plot_history(saved_history, ('acc', 'val_acc'))
+
+
+plot([saved_history['acc'][:epoch_unit], history.history['acc']], ['old', 'new'])
+plot([saved_history['val_acc'][:epoch_unit], history.history['val_acc']], ['old', 'new'])
 
 
 # Save Model
