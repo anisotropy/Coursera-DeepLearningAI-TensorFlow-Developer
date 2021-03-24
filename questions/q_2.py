f96fbd2986bfca4e9596bfda82f9a3878a15d3bb
hodoug <j.hodoug@gmail.com>
2021-03-24 21:10:37 +0900

    Update q_2.py

--- questions/q_2.py
+++ questions/q_2.py
@@ -1,6 +1,5 @@
 # mnist = tf.keras.datasets.fashion_mnist
 
-
 import tensorflow as tf
 from tensorflow.keras import layers, Input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
@@ -8,6 +7,12 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 import os
 import numpy as np
 import matplotlib.pyplot as mpplot
+
+
+def clear_session():
+    tf.keras.backend.clear_session()
+    tf.random.set_seed(51)
+    np.random.seed(51)
 
 
 def flat_histories(histories):
@@ -29,6 +34,18 @@ def plot_history(history, metrics=('loss',)):
     mpplot.legend()
 
 
+def plot(ys, labels=None):
+    mpplot.figure(figsize=(10, 6))
+    x = range(len(ys[0]))
+    for i in range(len(ys)):
+        if labels is not None and len(labels) == len(ys):
+            label = labels[i]
+        else:
+            label = None
+        mpplot.plot(x, ys[i], label=label)
+    mpplot.legend()
+
+
 def callback_of_stop_training(condition, message='Cancel training...'):
     class StopTraining(tf.keras.callbacks.Callback):
         def on_epoch_end(self, epoch, logs=None):
@@ -39,7 +56,7 @@ def callback_of_stop_training(condition, message='Cancel training...'):
     return StopTraining()
 
 
-# Get Data
+# Prepare Data
 def data_from_mnist(mnist, rescale):
     (images_train, labels_train), (images_valid, labels_valid) = mnist.load_data()
 
@@ -75,52 +92,46 @@ def image_generators(x_train, y_train, x_valid, y_valid, rescale, batch_size):
 
 
 x_train, y_train, x_valid, y_valid, num_class, image_shape = \
-    data_from_mnist(tf.keras.datasets.fashion_mnist, rescale=1.0)
+    data_from_mnist(tf.keras.datasets.fashion_mnist, rescale=1)
 
 gen_train, gen_valid = \
     image_generators(x_train, y_train, x_valid, y_valid, rescale=1/255, batch_size=32)
 
-input_shape = (28, 28, 1)
-
 
 # Model
 model = tf.keras.models.Sequential([
-    Input(shape=input_shape),
-    layers.Conv2D(16, (3, 3), activation='relu', ),
+    Input(shape=image_shape),
+    layers.Conv2D(32, (3, 3), activation='relu', ),
     layers.MaxPooling2D(pool_size=(2, 2)),
-    # layers.Conv2D(32, (3, 3), activation='relu'),
-    # layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D(pool_size=(2, 2)),
     # layers.Conv2D(64, (3, 3), activation='relu'),
     # layers.MaxPooling2D(pool_size=(2, 2)),
     # layers.Conv2D(64, (3, 3), activation='relu'),
     # layers.MaxPooling2D(pool_size=(2, 2)),
     layers.Flatten(),
-    layers.Dense(1024, activation='relu'),
+    layers.Dense(64, activation='relu'),
     # layers.Dropout(0.2),
-    layers.Dense(10, activation='softmax')
+    layers.Dense(num_class, activation='softmax')
 ])
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['acc']
-)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
 histories = []
 
 
 # Train
 stop_training = callback_of_stop_training(lambda logs: logs.get('acc') > 0.8 and logs.get('val_acc') > 0.8)
-history = model.fit(gen_train, validation_data=gen_valid, epochs=5, callbacks=[stop_training])
+history = model.fit(gen_train, validation_data=gen_valid, epochs=10, callbacks=[stop_training])
 histories.append(history.history)
+plot_history(history.history, ('acc', 'val_acc'))
 
 
 # History
 saved_history = flat_histories(histories)
 plot_history(saved_history, ('acc', 'val_acc'))
-plot_history(saved_history, ('loss', 'val_loss'))
+
+plot([saved_history['acc'][:10], history.history['acc']], ['old', 'new'])
 
 
 # Save Model
-model.save('model_q2_1.h5')
-
-
+model.save('model_q2_2.h5')
 
