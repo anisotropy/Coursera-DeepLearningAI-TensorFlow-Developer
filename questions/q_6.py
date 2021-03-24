d17aaf9b4ee6a630126c2abde00ab5ac838a2a3a
hodoug <j.hodoug@gmail.com>
2021-03-24 21:12:49 +0900

    Update q_6.py

--- questions/q_6.py
+++ questions/q_6.py
@@ -1,4 +1,4 @@
-# sign-mnist
+# sign-mnist.csv
 
 import tensorflow as tf
 from tensorflow.keras import layers, Input
@@ -6,105 +6,16 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, i
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 import os
 import numpy as np
-import csv
 import matplotlib.pyplot as mpplot
+import csv
 
 
-# Prepare Data
-def reshape_1d(values_1d, shape):
-    return np.array(values_1d, dtype='float32').reshape(shape)
+def clear_session():
+    tf.keras.backend.clear_session()
+    tf.random.set_seed(51)
+    np.random.seed(51)
 
 
-def image_generators(x_train, y_train, x_valid, y_valid, rescale, batch_size):
-    datagen_train = ImageDataGenerator(
-        rescale=rescale,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        rotation_range=40,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True
-    )
-    gen_train = datagen_train.flow(x_train, y_train, batch_size=batch_size, shuffle=True)
-
-    datagen_valid = ImageDataGenerator(rescale=rescale)
-    gen_valid = datagen_valid.flow(x_valid, y_valid, batch_size=batch_size, shuffle=True)
-
-    return gen_train, gen_valid
-
-
-x_train, y_train = [], []
-x_valid, y_valid = [], []
-target_size = (28, 28)
-input_shape = (28, 28, 1)
-
-num_classes = 25
-
-with open('tmp/sign-mnist/sign_mnist_train.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        # y_train.append(tf.keras.utils.to_categorical(int(row[0]), num_classes))
-        # values = np.array(row[1:], dtype='float32').reshape(target_size)
-        # x_train.append(np.expand_dims(values, axis=-1))
-        y_train.append(int(row[0]))
-        x_train.append(np.array(row[1:], dtype='float32').reshape(input_shape))
-
-with open('tmp/sign-mnist/sign_mnist_test.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        y_valid.append(int(row[0]))
-        x_valid.append(np.array(row[1:], dtype='float32').reshape(input_shape))
-
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_valid = np.array(x_valid)
-y_valid = np.array(y_valid)
-
-gen_train, gen_valid = image_generators(x_train, y_train, x_valid, y_valid, rescale=1/255, batch_size=32)
-
-
-# Model
-model = tf.keras.models.Sequential([
-    Input(shape=input_shape),
-    layers.Conv2D(32, (3, 3), activation='relu'),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Flatten(),
-    layers.Dense(512, activation='relu'),
-    # layers.Dropout(0.2),
-    layers.Dense(num_classes, activation='softmax')
-])
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['acc']
-)
-
-histories = []
-
-
-# Train
-def callback_of_stop_training(condition, message='Cancel training...'):
-    class StopTraining(tf.keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs=None):
-            if condition(logs):
-                print('\n{}'.format(message))
-                self.model.stop_training = True
-
-    return StopTraining()
-
-
-stop_training = callback_of_stop_training(
-    lambda logs: logs.get('acc') > 0.9 and logs.get('val_acc') > 0.9
-)
-history = model.fit(gen_train, validation_data=gen_valid, epochs=50, callbacks=[stop_training])
-histories.append(history.history)
-
-
-# History
 def flat_histories(histories):
     history = {}
     for h in histories:
@@ -124,9 +35,116 @@ def plot_history(history, metrics=('loss',)):
     mpplot.legend()
 
 
-my_history = flat_histories(histories)
-plot_history(my_history, ('acc', 'val_acc'))
+def plot(ys, labels=None):
+    mpplot.figure(figsize=(10, 6))
+    x = range(len(ys[0]))
+    for i in range(len(ys)):
+        if labels is not None and len(labels) == len(ys):
+            label = labels[i]
+        else:
+            label = None
+        mpplot.plot(x, ys[i], label=label)
+    mpplot.legend()
 
 
+def callback_of_stop_training(condition, message='Cancel training...'):
+    class StopTraining(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            if condition(logs):
+                print('\n{}'.format(message))
+                self.model.stop_training = True
+
+    return StopTraining()
 
 
+# Get Data
+def reshape_1d(values_1d, shape):
+    return np.array(values_1d, dtype='float32').reshape(shape)
+
+
+image_shape = (28, 28, 1)
+labels_train, image_train = [], []
+labels_test, image_test = [], []
+
+with open('tmp/sign-mnist/sign_mnist_train.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        labels_train.append(int(row[0]))
+        image_train.append(reshape_1d(row[1:], image_shape))
+
+with open('tmp/sign-mnist/sign_mnist_test.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        labels_test.append(int(row[0]))
+        image_test.append(reshape_1d(row[1:], image_shape))
+
+
+x_train = np.array(image_train)
+y_train = np.array(labels_train)
+x_valid = np.array(image_test)
+y_valid = np.array(labels_test)
+
+
+# Prepare Data
+def image_generators(x_train, y_train, x_valid, y_valid, rescale, batch_size):
+    datagen_train = ImageDataGenerator(
+        rescale=rescale,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rotation_range=40,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+    gen_train = datagen_train.flow(x_train, y_train, batch_size=batch_size, shuffle=True)
+
+    datagen_valid = ImageDataGenerator(rescale=rescale)
+    gen_valid = datagen_valid.flow(x_valid, y_valid, batch_size=batch_size, shuffle=True)
+
+    return gen_train, gen_valid
+
+
+gen_train, gen_valid = image_generators(
+    x_train, y_train, x_valid, y_valid,
+    rescale=1/255, batch_size=32
+)
+
+
+# Model -- new
+model = tf.keras.models.Sequential([
+    Input(shape=image_shape),
+    layers.Conv2D(32, (3, 3), activation='relu', ),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    # layers.Conv2D(32, (3, 3), activation='relu'),
+    # layers.MaxPooling2D(pool_size=(2, 2)),
+    # layers.Conv2D(64, (3, 3), activation='relu'),
+    # layers.MaxPooling2D(pool_size=(2, 2)),
+    # layers.Conv2D(64, (3, 3), activation='relu'),
+    # layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dense(1024, activation='relu'),
+    # layers.Dropout(0.2),
+    layers.Dense(25, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+histories = []
+
+
+# Train
+stop_training = callback_of_stop_training(lambda logs: logs.get('acc') > 0.8 and logs.get('val_acc') > 0.8)
+history = model.fit(gen_train, validation_data=gen_valid, epochs=10, callbacks=[stop_training])
+histories.append(history.history)
+plot_history(history.history, ('acc', 'val_acc'))
+
+
+# History
+saved_history = flat_histories(histories)
+plot_history(saved_history, ('acc', 'val_acc'))
+
+plot([saved_history['val_acc'][:10], history.history['val_acc']], ['old', 'new'])
+
+
+# Save Model
+model.save('model_q6_1.h5')
